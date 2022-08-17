7d0673c830c32e28c174360a0712f49f7524b62e
VIktor <viktor345@inbox.lv>
2022-08-17 20:05:11 +0300

    started my work with Python

---
+++ data_input_output_2_2_8.py
@@ -0,0 +1,6 @@
+print('Как тебя зовут?')
+name = input()
+print('Привет,', name)
+
+name = input('Как тебя зовут?')
+print('Привет,', name)
---
+++ data_input_output_sep_end.py
@@ -0,0 +1,13 @@
+print('a', 'b', 'c', sep='*')
+print('d', 'e', 'f', sep='**')
+
+print('a', 'b', 'c', end='@')
+print('d', 'e', 'f', end='\n')
+
+print('Python', end='\n\n\n')
+
+print('a', 'b', 'c', sep='*', end='finish')
+print('d', 'e', 'f', sep='**', end='^__^')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='#')
+print('m', 'n', 'o', sep='/', end='!')
