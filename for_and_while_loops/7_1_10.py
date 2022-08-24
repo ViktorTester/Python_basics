358452681795ff9676817e18f8753db01cb2f7ff
VIktor <viktor345@inbox.lv>
2022-08-24 19:15:34 +0300

    added 7_2_9 and 7_2_11 to for_and_while_loops

--- for_and_while_loops/7_1_10.py
+++ for_and_while_loops/7_1_10.py
@@ -4,6 +4,7 @@ print("1", m)
 for i in range(n - 1):
     m = m * x
     print(i + 2, m)
+
 # The program predicts the size of an organism's population.
 # m - the initial number of organisms;
 # p - average daily increase in %;
---
+++ for_and_while_loops/7_2_9.py
@@ -0,0 +1,17 @@
+m, n = int(input()), int(input())
+if m % 2 == 0:
+    for i in range(m - 1, n - 1, -2):
+        print(i)
+elif m % 2 == 1:
+    for i in range(m, n - 1, -2):
+        print(i)
+
+# same without if:
+
+m, n = int(input()), int(input())
+m = m % 2 + m - 1
+for i in range(m, n - 1, -2):
+    print(i)
+
+# Two integers m and n (m > nm>n) are given.
+# The program displays all odd numbers from m to n inclusive in descending order.
